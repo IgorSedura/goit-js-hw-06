1cc9b55aae44ffc0b3457aa60fd64f978c4ee163
Igor Sedura <igorsedura@gmail.com>
2022-09-12 15:26:31 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
-
+// Обработка отправки формы form.login-form должна быть по событию submit.
+// При отправке формы страница не должна перезагружаться.
+// Если в форме есть незаполненные поля, выводи alert с предупреждением о том, что все поля должны быть заполнены.
+// Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства, а значение поля - значением свойства. Для доступа к элементам формы используй свойство elements.
+// Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
+const formRef = document.querySelector(".login-form");
+const hendleSubmit = (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.target;
+  if (email.value === "" || password.value === "") {
+    alert("Заповніть всі поля");
+    return;
+  }
+  const userData = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(userData);
+  formRef.reset();
+};
+formRef.addEventListener("submit", hendleSubmit);
